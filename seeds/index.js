5dd4e49e775ca85704971b6d9e5595b77366876c
Becka McNally <beckamcnally@gmail.com>
2023-02-08 09:29:35 -0800

    seeds framework

---
+++ seeds/index.js
@@ -0,0 +1,14 @@
+const sequelize = require('../config/connection');
+const { Posts, Users } = require('../models');
+
+const PostsSeedData = require('./PostsSeedData.json');
+const UsersSeedData = require('./UsersSeedData.json');
+
+
+const seedDatabase = () => {
+  
+}
+
+
+
+seedDatabase();
